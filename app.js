f857ba542f46d9f7050e14f284ff79ce98e56211
Evil2l <dWill@yandex.ru>
2017-02-14 21:34:59 +0200

    args added

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 
 
-var request = require('request');
+const request = require('request');
+const yargs = require('yargs');
+const argv = yargs
+    .options({
+        a: {
+            demanded: true,
+            alias: 'address',
+            describe: 'address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+console.log(argv);
+let encodedAddress = encodeURIComponent(argv.a);
+console.log(encodedAddress);
+
+request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=+${encodedAddress}`,
+    json: true
+}, (err, response, body) => {
+    if(err){
+        console.error(`${err}`);
+    }else if(body.status === 'ZERO_RESULTS'){
+        console.error(`Unable to find that address`);
+    }
+    else if(body.status === 'OK'){
+        console.log(`Address: ${body.results[0].geometry.location.lat}`);
+    }
+});
